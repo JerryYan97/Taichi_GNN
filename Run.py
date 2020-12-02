a10d52e0364f606fad6e291b79b201ad81e7ba6c
Hello <yanjiarui97@126.com>
2020-12-02 16:31:44 -0500

    different angle experiments

--- Run.py
+++ Run.py
@@ -12,6 +12,8 @@ pn = PNSimulation(int(1), 2)
 running_times = 1
 frame_count = 50
 
+# NOTE: Please remember to save your data. It will delete all files in Outputs/ or Outputs_T/ when you exe Run.py.
+
 # pd->pn
 if __name__ == '__main__':
 
@@ -20,16 +22,26 @@ if __name__ == '__main__':
     if is_test == 0:
         if not os.path.exists("Outputs"):
             os.makedirs("Outputs")
+        for root, dirs, files in os.walk("Outputs/"):
+            for name in files:
+                os.remove(os.path.join(root, name))
     else:
         if not os.path.exists("Outputs_T"):
             os.makedirs("Outputs_T")
+        for root, dirs, files in os.walk("Outputs_T/"):
+            for name in files:
+                os.remove(os.path.join(root, name))
+
+
 
     for i in range(running_times):
         # pn.generate_exforce()
         # pn.compute_exforce(pn.exf_ind, pn.mag_ind)
         # pd.set_force(pn.exf_ind, pn.mag_ind)
-        pn.set_force(0, 3)
-        pd.set_force(0, 3)
+        # pn.set_force(0, 3)
+        # pd.set_force(0, 3)
+        pn.set_force(-45, 3)
+        pd.set_force(-45, 3)
 
         pd.set_material(rho, E, nu, dt)
         pn.set_material(rho, E, nu, dt)
