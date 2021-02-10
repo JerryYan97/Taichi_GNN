Fix Numerical Boom

src/Simulators/PDSimulator.py
@@ -7,6 +7,7 @@ from numpy.linalg import inv
 from scipy.linalg import sqrtm
 from .SimulatorBase import SimulatorBase
 import multiprocessing as mp
+from numpy import linalg as LA
 sys.path.append(os.path.dirname(os.path.abspath(__file__)) + "/../")
 from Utils.math_tools import svd
 from Utils.utils_visualization import draw_image, update_boundary_mesh, output_3d_seq
@@ -98,8 +99,6 @@ class PDSimulation(SimulatorBase):
         self.solver_max_iteration = 10
         self.solver_stop_residual = 1e20
         self.stop_acceleration = 0.04
-
-        self.damping_coeff = 0.0
 
         # Simulator Fields
         self.ti_volume = ti.field(self.real, self.n_elements)
@@ -447,7 +446,6 @@ class PDSimulation(SimulatorBase):
             local_offset_idx = 0
             for d in range(self.dim):
                 cur_sparse_val = 0.0
-                # cur_sparse_val += (-self.damping_coeff/self.dt)+(self.ti_mass[i]/(self.dt*self.dt))
                 cur_sparse_val += (self.ti_mass[i] / (self.dt * self.dt))
                 if self.ti_boundary_labels[i] == 1:
                     cur_sparse_val += self.m_weight_positional
@@ -544,11 +542,11 @@ class PDSimulation(SimulatorBase):
             Sn_idx2 = v_id * self.dim + 1
             pos_i = self.ti_x[v_id]
             vel_i = self.ti_vel[v_id]
-            self.ti_Sn[Sn_idx1] = pos_i[0] + self.dt * vel_i[0] + (self.dt ** 2) * (self.ti_ex_acc[v_id][0] - vel_i[0]*self.damping_coeff/self.ti_mass[v_id])
-            self.ti_Sn[Sn_idx2] = pos_i[1] + self.dt * vel_i[1] + (self.dt ** 2) * (self.ti_ex_acc[v_id][1] - vel_i[1]*self.damping_coeff/self.ti_mass[v_id])
+            self.ti_Sn[Sn_idx1] = pos_i[0] + self.dt * vel_i[0] + (self.dt ** 2) * (self.ti_ex_acc[v_id][0])
+            self.ti_Sn[Sn_idx2] = pos_i[1] + self.dt * vel_i[1] + (self.dt ** 2) * (self.ti_ex_acc[v_id][1])
             if ti.static(self.dim == 3):
                 Sn_idx3 = v_id * self.dim + 2
-                self.ti_Sn[Sn_idx3] = pos_i[2] + self.dt * vel_i[2] + (self.dt ** 2) * (self.ti_ex_acc[v_id][2] - vel_i[2]*self.damping_coeff/self.ti_mass[v_id])
+                self.ti_Sn[Sn_idx3] = pos_i[2] + self.dt * vel_i[2] + (self.dt ** 2) * (self.ti_ex_acc[v_id][2])
 
     @ti.func
     def Build_Bp_i_vec(self, idx):
@@ -569,9 +567,6 @@ class PDSimulation(SimulatorBase):
         # Construct the first part of the rhs
         for i in range(self.n_vertices * self.dim):
             rhs[i] = one_over_dt2 * self.ti_mass[i // self.dim] * self.ti_Sn[i]
-        # for i in ti.static(range(self.n_vertices * self.dim)):
-        #     rhs[i] = one_over_dt2 * self.ti_mass[i // self.dim] * self.ti_Sn[i] - \
-        #              (self.damping_coeff / self.dt * dp_pos[i // self.dim, i % self.dim])
         # Add strain and volume/area constraints to the rhs
         for t in ti.static(range(2)):
             for ele_idx in range(self.n_elements):
@@ -838,6 +833,7 @@ class PDSimulation(SimulatorBase):
 
         self.init_mesh_DmInv(self.dirichlet, len(self.dirichlet))
         self.precomputation(lhs_mat_row, lhs_mat_col, lhs_mat_val)
+
         s_lhs_matrix_np = sparse.csr_matrix((lhs_mat_val, (lhs_mat_row, lhs_mat_col)),
                                             shape=(self.n_vertices * self.dim, self.n_vertices * self.dim),
                                             dtype=np.float64)
@@ -1006,14 +1002,17 @@ class PDSimulation(SimulatorBase):
             # print("pd solve time: ", pd_end_t - pd_start_t)
 
             self.update_velocity_pos()
-            # self.gradE.from_numpy(pn.get_gradE_from_pd(self.ti_x))
             gradE = pn.get_gradE_from_pd(self.ti_x)
             # t_out_start = time.time()
-            if frame_counter % 10 == 0:
-                self.output_network_data(self.ti_x_del.to_numpy(),
-                                         pn_dis.to_numpy(),
-                                         gradE, init_rel_pos,
-                                         frame_counter, is_test)
+            # if frame_counter % 10 == 0:
+            #     self.output_network_data(self.ti_x_del.to_numpy(),
+            #                              pn_dis.to_numpy(),
+            #                              gradE, init_rel_pos,
+            #                              frame_counter, is_test)
+            self.output_network_data(self.ti_x_del.to_numpy(),
+                                     pn_dis.to_numpy(),
+                                     gradE, init_rel_pos,
+                                     frame_counter, is_test)
             # t_out_end = time.time()
             # print("output network data time: ", t_out_end - t_out_start)
 

src/Utils/reader.py
@@ -273,8 +273,15 @@ def read(testcase):
         mesh = pymesh.load_mesh(
             os.path.dirname(os.path.abspath(__file__)) + "/../../MeshModels/armadillolow_17698v_72161t.msh")
         dirichlet_list = []
+        # Fix bottom:
+        # for i in range(mesh.num_vertices):
+        #     if mesh.vertices[i][1] <= mesh.bbox[0][1] + 0.01:
+        #         dirichlet_list.append(i)
+
+        # Fix left hand:
+        vert8_pos = mesh.vertices[8]
         for i in range(mesh.num_vertices):
-            if mesh.vertices[i][1] <= mesh.bbox[0][1] + 0.01:
+            if LA.norm(vert8_pos - mesh.vertices[i]) < 0.2:
                 dirichlet_list.append(i)
 
         dirichlet = np.array(dirichlet_list)
@@ -347,6 +354,12 @@ def read(testcase):
         for i in range(mesh.num_vertices):
             if mesh.vertices[i][1] <= mesh.bbox[0][1] + 0.01:
                 dirichlet_list.append(i)
+
+        # Fix left hand:
+        # vert8_pos = mesh.vertices[8]
+        # for i in range(mesh.num_vertices):
+        #     if LA.norm(vert8_pos - mesh.vertices[i]) < 0.2:
+        #         dirichlet_list.append(i)
 
         dirichlet = np.array(dirichlet_list)
         mesh_scale = 1 / (np.amax(mesh.vertices) - np.amin(mesh.vertices)) * 0.3

src/tools/pd_standalone.py
@@ -12,7 +12,7 @@ from Utils.math_tools import svd, my_svd
 real = ti.f64
 
 # Mesh load and test case selection:
-test_case = 1010
+test_case = 1007
 case_info = read(test_case)
 mesh = case_info['mesh']
 dirichlet = case_info['dirichlet']
@@ -40,8 +40,11 @@ else:
     boundary_pos = np.ndarray(shape=(case_info['boundary_tri_num'], 3, 3), dtype=np.float)
 
 # Material settings:
-rho = 1e3
-E, nu = 5e6, 0.4  # Young's modulus and Poisson's ratio
+# rho = 1e3
+# E, nu = 5e6, 0.4  # Young's modulus and Poisson's ratio
+E = 0.01e9
+nu = 0.49
+rho = 1.1e3
 mu, lam = E / (2*(1+nu)), E * nu / ((1+nu)*(1-2*nu))  # Lame parameters
 
 
@@ -51,7 +54,7 @@ dt = 0.01
 # Backup settings:
 # Bar: 10  Bunny: 50
 solver_max_iteration = 1
-solver_stop_residual = 0.001
+solver_stop_residual = 1e20
 # external acceleration -- counter-clock wise
 ti_ex_acc = ti.Vector.field(dim, real, n_vertices)
 
@@ -130,7 +133,7 @@ def set_exacc():
     else:
         for i in range(n_vertices):
             # ti_ex_acc[i] = ti.Vector([0.0, -12.0, 12.0])
-            ti_ex_acc[i] = ti.Vector([0.0, 0.0, 0.0])
+            ti_ex_acc[i] = ti.Vector([0.0, -980.0, 0.0])
 
 
 @ti.kernel
@@ -425,7 +428,7 @@ def compute_volume_constraint(sigma):
         return ti.Matrix.rows([[sigma_star_11_k, 0.0], [0.0, sigma_star_22_k]])
     else:
         tol = 0.00001
-        max_it = 2
+        max_it = 1
         s1, s2, s3 = sigma[0, 0], sigma[1, 1], sigma[2, 2]
         x = 1.0 - s1
         y = 1.0 - s2
@@ -778,7 +781,7 @@ if __name__ == "__main__":
     pre_fact_lhs_solve = factorized(s_lhs_matrix_np)
 
     # wait = input("PRESS ENTER TO CONTINUE.")
-    animation_init()
+    # animation_init()
     # mag = 16.0
     # set_point_acc_by_point_3D(1, 0.1, mag*-1.0, mag*0.0, mag*0.0)
 
@@ -807,9 +810,9 @@ if __name__ == "__main__":
         warm_up()
         print("Frame ", frame_counter)
         # last_record_energy = 1000000.0
-        animation_control(frame_counter)
-        for itr in range(solver_max_iteration):
-            # while True:
+        # animation_control(frame_counter)
+        # for itr in range(solver_max_iteration):
+        while True:
             local_solve_build_bp_for_all_constraints()
             build_rhs(rhs_np)
 