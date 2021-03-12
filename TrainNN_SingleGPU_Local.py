416e5cb0df779774169ea40db41e4dcaa7ee533e
Hello <yanjiarui97@126.com>
2021-03-12 10:31:21 -0500

    two more NN

--- TrainNN_SingleGPU_Local.py
+++ TrainNN_SingleGPU_Local.py
@@ -7,7 +7,8 @@ import torch.optim as optim
 from src.Utils.utils_gcn import *
 # from src.NeuralNetworks.LocalNN.VertNN_Feb28_LocalLinear import *
 # from src.NeuralNetworks.LocalNN.VertNN_Mar2_Local import *
-from src.NeuralNetworks.LocalNN.VertNN_Mar10_Local_Wider import *
+# from src.NeuralNetworks.LocalNN.VertNN_Mar12_Local_Simple import *
+from src.NeuralNetworks.LocalNN.VertNN_Mar12_Local import *
 from torch.utils.data import DataLoader
 from torch.utils.tensorboard import SummaryWriter
 from torch.optim.lr_scheduler import ReduceLROnPlateau
@@ -23,7 +24,7 @@ writer = SummaryWriter('../runs/GCN_Local_1009_single')
 ###################################################
 
 # Training settings
-epoch_num = 500
+epoch_num = 300
 parser = argparse.ArgumentParser()
 parser.add_argument('--no-cuda', action='store_true', default=False, help='Disables CUDA training.')
 parser.add_argument('--seed', type=int, default=1345, help='Random seed.')
@@ -54,7 +55,7 @@ print("data load time:", load_data_t_end - load_data_t_start)
 dim = case_info['dim']
 
 train_loader = DataLoader(dataset=simDataset,
-                          batch_size=1024,
+                          batch_size=512,
                           shuffle=True,
                           num_workers=os.cpu_count(),
                           pin_memory=True)
@@ -66,7 +67,7 @@ train_loader = DataLoader(dataset=simDataset,
 #     device=device
 # ).to(device)
 
-model = VertNN_Mar10_LocalLinear_Wider(
+model = VertNN_Mar12_LocalLinear(
     nfeat=simDataset.input_features_num,
     fc_out=simDataset.output_features_num,
     dropout=0,
